854b9fb805f4acb2581688a97f45e7aae3727034
Andrés Caro <andres.caro.q@gmail.com>
2016-07-24 04:26:16 -0400

    Completed Assignment #1

--- assignments/recfun/src/main/scala/recfun/Main.scala
+++ assignments/recfun/src/main/scala/recfun/Main.scala
@@ -13,15 +13,35 @@ object Main {
   /**
    * Exercise 1
    */
-    def pascal(c: Int, r: Int): Int = ???
+    def pascal(c: Int, r: Int): Int = {
+      if (c == 0 || c == r) 1 else pascal(c - 1, r - 1) + pascal(c, r - 1)
+    }
   
   /**
    * Exercise 2
    */
-    def balance(chars: List[Char]): Boolean = ???
+    def balance(chars: List[Char]): Boolean = {
+      def traverseChars(chars: List[Char], balanced: List[Boolean]): Boolean = {
+        if (chars.isEmpty) balanced.isEmpty
+        else if (chars.head == '(') traverseChars(chars.tail, false :: balanced)
+        else if
+          (chars.head == ')') traverseChars(chars.tail, if (balanced.isEmpty) false :: balanced else balanced.tail)
+        else traverseChars(chars.tail, balanced)
+      }
+
+      traverseChars(chars, List[Boolean]())
+    }
   
   /**
    * Exercise 3
    */
-    def countChange(money: Int, coins: List[Int]): Int = ???
+    def countChange(money: Int, coins: List[Int]): Int = {
+      def count(money: Int, k: Int): Int = {
+        if (k == 0 || money < 0) 0
+        else if (money == 0) 1
+        else count(money, k - 1) + count(money - coins(k - 1), k)
+      }
+
+      count(money, coins.size)
+    }
   }
